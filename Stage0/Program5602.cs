80cd016f1b138a04bccf06091f260fd86116be43
ChayaRubin <ct3227200@gmail.com>
2024-10-27 22:27:29 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program5602.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+     
+        static partial void Welcome5602()
+        { 
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
