ce04e1411f6166defc8b47ae5bb80f1d6cef837b
ChayaRubin <ct3227200@gmail.com>
2024-11-09 23:17:45 +0200

    הי

---
+++ DalFacade/DO/Volunteer.cs
@@ -0,0 +1,25 @@
+﻿namespace DO;
+
+
+/// <summary>
+/// Student Entity represents a student with all its props
+/// </summary>
+/// <param name="Id">Personal unique ID of the student (as in national id card)</param>
+/// <param name="Name">Private Name of the student</param>
+
+public record Volunteer
+(
+    int Id,
+    string Name,
+    string Phone,
+    string Email,
+    string? Password,
+    string? Address,
+    double? Latitude,
+    double? Longitude,
+    Enum Position,
+    bool Active,
+    double? MaxResponseDistance,
+   Enum TypeOfDistince
+);
+
--- DalList/config.cs
+++ DalList/config.cs
@@ -30,3 +30,6 @@ internal static class Config
 
 
 
+
+
+
