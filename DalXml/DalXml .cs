3a0b348839aa8084feff2388b4c918bbe37786ef
ChayaRubin <ct3227200@gmail.com>
2024-12-19 00:48:21 +0200

    also DalXml page (7)

--- DalXml/DalXml .cs
+++ DalXml/DalXml .cs
@@ -3,6 +3,18 @@
 //namespace Dal;
 using DalApi;
 namespace Dal;
-public class DalXml: IDal
+public class DalXml : IDal
 {
+    public IAssignment Assignment => throw new NotImplementedException();
+
+    public IVolunteer Volunteer => throw new NotImplementedException();
+
+    public ICall Call => throw new NotImplementedException();
+
+    public IConfig Config => throw new NotImplementedException();
+
+    public void ResetDB()
+    {
+        throw new NotImplementedException();
+    }
 }
