04d07732f9e19debaf579de2cdce14653e159fa5
ChayaRubin <ct3227200@gmail.com>
2024-12-21 21:28:29 +0200

    assignment

--- DalXml/AssignmentImplementation.cs
+++ DalXml/AssignmentImplementation.cs
@@ -4,36 +4,48 @@ using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 internal class AssignmentImplementation : IAssignment
 {
-    public void Create(Assignment item)
+    static Assignments getAssignment(XElement a)
     {
-        throw new NotImplementedException();
+        return new DO.Assignment()
+        {
+            Id = a.ToIntNullable("Id") ?? throw new FormatException("can't convert id"),
+            CallId = a.ToIntNullable("CallId") ?? throw new FormatException("Can't convert CallId"),
+            VolunteerId = a.ToIntNullable("VolunteerId") ?? throw new FormatException("Can't convert CallId"),
+            EntryTime = a.ToDateTimeNullable("EntryTime") ?? throw new FormatException("Can't convert EntryTime"),
+            FinishCompletionTime = a.ToDateTimeNullable("FinishCompletionTime") ?? throw new FormatException("Can't convert EntryTime"),
+            callResolutionStatus = a.ToEnumNullable<CallResolutionStatus>("callResolutionStatus") ?? throw new FormatException("Can't convert callResolutionStatus")
+        };
+
     }
 
-    public void Delete(int id)
-    {
-        throw new NotImplementedException();
-    }
+        public Assignment? Read(int id)
+        {
+            XElement? studentElem =
+        XMLTools.LoadListFromXMLElement(Config.s_assignment_xml).Elements().FirstOrDefault(st => (int?)st.Element("Id") == id);
+            return studentElem is null ? null : getAssignment(studentElem);
+        }
 
-    public void DeleteAll()
-    {
-        throw new NotImplementedException();
-    }
+        public Assignment? Read(Func<Assignment, bool> filter)
+        {
+            return XMLTools.LoadListFromXMLElement(Config.s_assignment_xml).Elements().Select(s => getAssignment(s)).FirstOrDefault(filter);
+        }
 
-    public Assignment? Read(Func<Assignment, bool> filter)
-    {
-        throw new NotImplementedException();
-    }
+        public void Update(Assignment item)
+        {
+            XElement studentsRootElem = XMLTools.LoadListFromXMLElement(Config.s_assignment_xml);
 
-    public IEnumerable<Assignment> ReadAll(Func<Assignment, bool>? filter = null)
-    {
-        throw new NotImplementedException();
-    }
+            (studentsRootElem.Elements().FirstOrDefault(st => (int?)st.Element("Id") == item.Id)
+            ?? throw new DO.DalDoesNotExistException($"Student with ID={item.Id} does Not exist"))
+                    .Remove();
 
-    public void Update(Assignment item)
-    {
-        throw new NotImplementedException();
-    }
+            studentsRootElem.Add(new XElement("Student", createAssignmentElement(item)));
+
+            XMLTools.SaveListToXMLElement(studentsRootElem, Config.s_assignment_xml);
+        }
+    
+
 }
